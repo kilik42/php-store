dd339fbae55354e7fde9a44ebc0a34fbb0dc8ce2
kilik42 <marvinevins@yahoo.com>
2020-04-19 12:56:53 -0500

    php store

---
+++ assets/includes/footer.php
@@ -0,0 +1,41 @@
+
+
+<div id="footer" class="cf">
+    <div class="column-three">
+        <strong>Phone</strong>
+        808.529.3819
+    </div>
+    <div class="column-three">
+        <strong>Location</strong>
+        123 Kapiolani Boulevard
+        Honolulu, HI
+
+    </div>
+    <div class="column-three last">
+
+        <strong>Hours</strong>
+        <em>Tuesday - Thursday</em>
+        1:00 pm - 9:00 pm <br><br>
+
+        <em>Friday - Saturday</em>
+        4:00 pm - 11:00 pm <br><br>
+
+        <em>Sunday - Monday</em>
+        Closed <br><br>
+
+    </div>
+</div>
+
+<small>&copy; 2104 Brad Hussey</small>
+
+</div>
+
+
+</div>
+
+<div class="copyright-info">
+    <?php include('../assets/includes/copyright.php'); ?>
+</div>
+
+</body>
+</html>
---
+++ assets/includes/header.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/style.css">
+    <style type="text/css">
+        body {
+            font-family: 'Helvetica Neue', arial;
+            text-align: center;
+        }
+        table {
+            font-size: small;
+            text-align: left;
+            margin: 100px auto 0 auto;
+            border-collapse: collapse;
+        }
+        td {
+            padding: 2px 8px;
+            border: 1px solid #ccc;
+        }
+    </style>
+    <title>Document</title>
+</head>
+<body  id="final-example">
+<div class="wrapper">
+    <div id="banner">
+        <a href="img/banner.png"title="return to home"></a>
+    </div>
+    <div id="nav">
+
+           <?php include ('assets/includes/nav.php');?>
+
+    </div>
+
+    <div id="content">
